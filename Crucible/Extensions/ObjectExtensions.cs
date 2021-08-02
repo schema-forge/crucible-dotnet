50f7d8671e8a7d188ada77bdfbe6225b3cbdd44b
CuroSalus <danieltholtzclaw@gmail.com>
2021-08-02 11:05:46 -0500

    Added common extensions.

---
+++ Crucible/Extensions/ObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schemaforge.Crucible.Extensions
+{
+  public static class ObjectExtensions
+  {
+    /// <summary>
+    /// Syntactic sugar to check for null.
+    /// </summary>
+    /// <param name="obj">Object to check it's null status.</param>
+    /// <returns>True if not null.</returns>
+    public static bool Exists(this object obj)
+    {
+      return obj is not null;
+    }
+
+    /// <summary>
+    /// Syntactic sugar to check for null.
+    /// </summary>
+    /// <typeparam name="T">Underlying value type.</typeparam>
+    /// <param name="strct">Object to check.</param>
+    /// <returns>True if not null, or false if it is.</returns>
+    public static bool Exists<T>(this T? strct)
+      where T : struct
+    {
+      return strct is not null;
+    }
+  }
+}
