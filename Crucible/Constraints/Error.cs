fc9ae28d540ba984d877cd7e7e3809c6b7ab4ab7
Sanity Dance <58183513+sanity-dance@users.noreply.github.com>
2021-08-02 21:48:19 -0700

    Added Error class and associated tests.

---
+++ Crucible/Constraints/Error.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using schemaforge.Crucible.Extensions;
+
+namespace schemaforge.Crucible
+{
+  public enum Severity
+  {
+    Warning,
+    Fatal,
+    NullOrEmpty,
+    Info,
+    Trace
+  }
+  public class Error
+  {
+    public string ErrorMessage { get; }
+    public Severity ErrorSeverity { get; }
+
+    public Error(string inputMessage, Severity inputSeverity = Severity.Fatal)
+    {
+      if(inputMessage.IsNullOrEmpty())
+      {
+        throw new ArgumentNullException(nameof(inputMessage),"inputMessage of Error cannot be null or whitespace.");
+      }
+      ErrorMessage = inputMessage;
+      ErrorSeverity = inputSeverity;
+    }
+
+    public override string ToString() => ErrorMessage;
+  }
+}
---
+++ OSHA/CrucibleTests/SchemaControllerTests/ErrorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using schemaforge.Crucible;
+using schemaforge.Crucible.Extensions;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace SchemaControllerTests
+{
+  public class ErrorTests
+  {
+    [Fact]
+    public void Error_ConstructorInitializesErrorMessage()
+    {
+      Error error = new("You dun goofed!");
+      Assert.Equal("You dun goofed!", error.ErrorMessage);
+      Assert.Equal(Severity.Fatal, error.ErrorSeverity);
+      Assert.Equal("You dun goofed!", error.ErrorMessage);
+    }
+
+    [Fact]
+    public void Error_ConstructorInitializesErrorSeverity()
+    {
+      Error error = new Error("You should not crossed the streams.", Severity.Warning);
+      Assert.Equal(Severity.Warning, error.ErrorSeverity);
+    }
+
+    [Fact]
+    public void Error_ConstructorThrowsOnEmptyString()
+    {
+      Assert.Throws<ArgumentNullException>(() => new Error(""));
+    }
+  }
+}
