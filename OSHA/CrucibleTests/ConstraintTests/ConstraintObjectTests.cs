c7dfa2ac6e94d7711b3cd34c517525113528daaf
Sanity Dance <58183513+sanity-dance@users.noreply.github.com>
2021-08-04 08:55:52 -0700

    Updated OSHA standards for ConstraintContainer and moved it to its own file.

--- OSHA/CrucibleTests/ConstraintTests/ConstraintObjectTests.cs
+++ OSHA/CrucibleTests/ConstraintTests/ConstraintObjectTests.cs
@@ -7,12 +7,20 @@ using SchemaForge.Crucible.Extensions;
 using Newtonsoft.Json.Linq;
 using Xunit;
 using Xunit.Abstractions;
+using static SchemaForge.Crucible.Constraints;
 
 namespace ConstraintTests
 {
   public class ConstraintObjectTests
   {
-    Func<JToken, string, List<Error>> TestFunction = (JToken input, string inputName) => new List<Error>();
+    /// <summary>
+    /// Empty (but valid) function to make testing easy.
+    /// </summary>
+    readonly Func<JToken, string, List<Error>> TestFunction = (JToken input, string inputName) => new List<Error>();
+
+    /// <summary>
+    /// Tests to ensure that the constructor populates JsonConstraints properly.
+    /// </summary>
     [Fact]
     public void ConstraintContainer_ConstructorValid()
     {
@@ -20,31 +28,88 @@ namespace ConstraintTests
       Assert.NotEmpty(newContainer.JsonConstraints);
     }
 
+    /// <summary>
+    /// Tests to ensure that a non-JObject in the passed JArray throws ArgumentException.
+    /// </summary>
     [Fact]
     public void ConstraintContainer_ConstructThrowsOnInvalidArray()
     {
       Assert.Throws<ArgumentException>(() => new ConstraintContainer(TestFunction, new JArray() { new JArray() }));
     }
 
+    /// <summary>
+    /// Tests to ensure that a JObject that does not contain Type throws ArgumentException.
+    /// </summary>
     [Fact]
     public void ConstraintContainer_ConstructorThrowsWhenMissingType()
     {
       Assert.Throws<ArgumentException>(() => new ConstraintContainer(TestFunction, new JArray() { new JObject() }));
     }
 
+    /// <summary>
+    /// Tests to ensure that ToString() returns one JObject when only one set of type-based constraints is passed.
+    /// </summary>
+    [Fact]
+    public void ConstraintContainer_ToString1ConstraintSet()
+    {
+      ConstraintContainer testContainer = ApplyConstraints<string>(ConstrainStringValues("Something", "or other"));
+      JObject expected = new()
+      {
+        { "Type", "System.String" },
+        { "ConstrainStringValues", new JArray() { "Something", "or other" } }
+      };
+
+      Assert.Equal(expected.ToString(), testContainer.ToString());
+    }
+
+    /// <summary>
+    /// Tests to ensure that ToString() returns two JObject string representations in an array when there are two sets of type-based constraints.
+    /// </summary>
+    [Fact]
+    public void ConstraintContainer_ToString2ConstraintSets()
+    {
+      ConstraintContainer testContainer = ApplyConstraints<int,string>(new Constraint[] { ConstrainNumericValue((5,15),(35,56)) },new Constraint[] { ConstrainStringValues("Something", "or other") });
+
+      JObject expected1 = new()
+      {
+        { "Type", "System.Int32" },
+        { "ConstrainNumericValue", new JArray() { "(5, 15)", "(35, 56)" } }
+      }; 
+      
+      JObject expected2 = new()
+      {
+        { "Type", "System.String" },
+        { "ConstrainStringValues", new JArray() { "Something", "or other" } }
+      };
+
+      JArray expected = new JArray() { expected1, expected2 };
+
+      Assert.Equal(expected.ToString(), testContainer.ToString());
+    }
+
+    /// <summary>
+    /// Ensures that the constructor populates Function and Property.
+    /// </summary>
     [Fact]
     public void Constraint_ConstructorValid()
     {
       Constraint constraint = new(TestFunction, new JProperty("ConstraintName", "Bill"));
       Assert.NotNull(constraint.Function);
+      Assert.NotNull(constraint.Property);
     }
 
+    /// <summary>
+    /// Ensures that the constructor throws ArgumentNullException when the name of the JProperty is empty.
+    /// </summary>
     [Fact]
     public void Constraint_ConstructorThrowsOnNullName()
     {
       Assert.Throws<ArgumentNullException>(() => new Constraint(TestFunction, new JProperty("", "Ghost Property")));
     }
 
+    /// <summary>
+    /// Ensures that the constructor throws ArgumentNullException when the value of the JProperty is empty.
+    /// </summary>
     [Fact]
     public void Constraint_ConstructorThrowsOnNullValue()
     {
