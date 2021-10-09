e47290abb204e8d99e5990f4d18da18fd22f5ce3
Sanity Dance <58183513+sanity-dance@users.noreply.github.com>
2021-10-09 13:42:23 -0700

    Enhanced OSHA compliance of JObjectTranslator.

--- OSHA/CrucibleTests/TestConfig.cs
+++ OSHA/CrucibleTests/TestConfig.cs
@@ -11,28 +11,41 @@ namespace Crucible
 {
   public class TestConfig
   {
-    TestConfig(JObject inputConfig)
+    public static Schema testSchema = new(new HashSet<ConfigToken>()
     {
-      Schema testSchema = new();
-      testSchema.AddTokens(new HashSet<ConfigToken>()
-            {
-                new ConfigToken<string>("RequiredToken","String: A Very Important Token[tm]"),
-                new ConfigToken<string>("AllowValues","String: Constrained string.",new Constraint<string>[] { AllowValues("This Is The End","(if you want it") }, required: false),
-                new ConfigToken<string>("ConstrainStringWithRegexExactPatterns","String: Constrained by Regex patterns [A-z] or [1-3].",new Constraint<string>[] { ConstrainStringWithRegexExact(new Regex("[A-z]"), new Regex("[1-3]")) }, required: false),
-                new ConfigToken<string>("ConstrainStringLengthLowerBound","String: Minimum length of 3.",new Constraint<string>[] { ConstrainStringLengthLowerBound(3) }, required: false),
-                new ConfigToken<string>("ConstrainStringLength","String: Length must be between 3 and 10.",new Constraint<string>[] { ConstrainStringLength(3,10) }, required: false),
-                new ConfigToken<string>("ForbidSubstrings","String: Characters / and * are forbidden.",new Constraint<string>[] { ForbidSubstrings("/","*") }, required: false),
-                new ConfigToken<int>("ConstrainValueLowerBound","Int: Number at least 10.",new Constraint<int>[] { ConstrainValueLowerBound(10) }, required: false),
-                new ConfigToken<int>("ConstrainValue","Int: Number at least 10 and at most 50.",new Constraint<int>[] { ConstrainValue(10,50) }, required: false),
-                new ConfigToken<int>("ConstrainValueDomains","Int: Within ranges 10-50 or 100-150.",new Constraint<int>[] { ConstrainValue((10, 50), (100, 150)) }, required: false),
-                //new ConfigToken("ConstrainJsonTokensRequired","Obligatory decoy Json.",false,ApplyConstraints<JObject>(ConstrainJsonTokens(new ConfigToken[] { new ConfigToken("RequiredInnerToken","String: Required inner token.",ApplyConstraints<string>())}))),
-                //new ConfigToken("ConstrainJsonTokens","Other obligatory decoy Json.",false,ApplyConstraints<JObject>(ConstrainJsonTokens(new ConfigToken[] { new ConfigToken("RequiredInnerToken","String: Required inner token.",ApplyConstraints<string>())},new ConfigToken[] { new ConfigToken("OptionalInnerToken","String: Optional inner token.",ApplyConstraints<string>()) }))),
-                new ConfigToken<JObject>("ConstrainCollectionCountLowerBound","Why have you scrolled this far? The heat of the forge sears me. I can no longer remember the coolness of a summer day.",new Constraint<JObject>[] { ConstrainCollectionCountLowerBound<JObject>(1) }, required: false),
-                new ConfigToken<JObject>("ConstrainCollectionCount","Burn with me.",new Constraint<JObject>[] { ConstrainCollectionCount<JObject>(1,3) }, required: false),
-                new ConfigToken<JArray>("ConstrainCollectionCountLowerBound","BURN WITH ME, MARTHA.",new Constraint<JArray>[] { ConstrainCollectionCountLowerBound<JArray>(1) }, required: false),
-                new ConfigToken<JArray>("ConstrainCollectionCount","Anything. Anything for just a moment of relief. Anything to lay my head upon sunbaked asphalt and feel its cold touch. Anything.",new Constraint<JArray>[] { ConstrainCollectionCount<JArray>(1,5) }, required: false),
-                new ConfigToken<JArray>("ApplyConstraintsToArrayElements","hurry",new Constraint<JArray>[] { ApplyConstraintsToJArray(AllowValues("under the smelters", "in the tunnels beneath", "help us")) }, required: false)
-            });
-    }
+        new ConfigToken<string>("RequiredToken","String: A Very Important Token[tm]"),
+        new ConfigToken<string>("Unrequired Token","String: Too lazy to show up.", required: false),
+        new ConfigToken<string>("AllowValues","String: Constrained string.",new Constraint<string>[] { AllowValues("This Is The End","(if you want it)") }),
+        new ConfigToken<string>("ConstrainStringWithRegexExactPatterns","String: Constrained by Regex patterns [A-z] or [1-3].",new Constraint<string>[] { ConstrainStringWithRegexExact(new Regex("[A-z]"), new Regex("[1-3]")) }),
+        new ConfigToken<string>("ConstrainStringLengthLowerBound", "String: Minimum length of 3.", new Constraint<string>[] { ConstrainStringLengthLowerBound(3) }),
+        new ConfigToken<string>("ConstrainStringLength", "String: Length must be between 3 and 10.", new Constraint<string>[] { ConstrainStringLength(3, 10) }),
+        new ConfigToken<string>("ForbidSubstrings", "String: Characters / and * are forbidden.", new Constraint<string>[] { ForbidSubstrings("/", "*") }),
+        new ConfigToken<int>("ConstrainValueLowerBound", "Int: Number at least 10.", new Constraint<int>[] { ConstrainValueLowerBound(10) }),
+        new ConfigToken<int>("ConstrainValue", "Int: Number at least 10 and at most 50.", new Constraint<int>[] { ConstrainValue(10, 50) }),
+        new ConfigToken<int>("ConstrainValueDomains", "Int: Within ranges 10-50 or 100-150.", new Constraint<int>[] { ConstrainValue((10, 50), (100, 150)) }),
+        new ConfigToken<JObject>("ConstrainCollectionCountLowerBound", "Why have you scrolled this far? The heat of the forge sears me. I can no longer remember the coolness of a summer day.", new Constraint<JObject>[] { ConstrainCollectionCountLowerBound<JObject>(1) }),
+        new ConfigToken<JObject>("ConstrainCollectionCount", "Burn with me.", new Constraint<JObject>[] { ConstrainCollectionCount<JObject>(1, 3) }),
+        new ConfigToken<JArray>("ConstrainArrayCountLowerBound", "BURN WITH ME, MARTHA.", new Constraint<JArray>[] { ConstrainCollectionCountLowerBound<JArray>(1) }),
+        new ConfigToken<JArray>("ConstrainArrayCount", "Anything. Anything for just a moment of relief. Anything to lay my head upon sunbaked asphalt and feel its cold touch. Anything.", new Constraint<JArray>[] { ConstrainCollectionCount<JArray>(1, 5) }),
+        new ConfigToken<JArray>("ApplyConstraintsToArrayElements", "hurry", new Constraint<JArray>[] { ApplyConstraintsToJArray(AllowValues("under the smelters", "in the tunnels beneath", "help us")) })
+    });
+
+    public static string testJson =
+  @"{
+      'RequiredToken':'Payable on delivery.',
+      'AllowValues':'This Is The End',
+      'ConstrainStringWithRegexExactPatterns':'32113',
+      'ConstrainStringLengthLowerBound':'Is this long enough, father?',
+      'ContrainStringLength':'Maybe.',
+      'ForbidSubstrings':'This string does not use any forbidden characters or substrings. Stop looking at it. I promise it contains none of the characters that have been forbidden. You're making it nervous by scrolling this far to the right. The string is self-conscious now, and it's all your fault. I hope you're happy.',
+      'ConstrainValueLowerBound':37,
+      'ConstrainValue':37,
+      'ConstrainValueDomains':37,
+      'ConstrainCollectionCountLowerBound':{'One item':'.', 'Two items':'..', 'Three items':'...'},
+      'ConstrainCollectionCount':{'One item':'.', 'Two items':'..', 'Three items':'...'},
+      'ConstrainArrayCountLowerBound':[1, 2, 3],
+      'ConstrainArrayCount':[4, 5, 6],
+      'ApplyConstraintsToArrayElements':['under the smelters', 'in the tunnels beneath', 'help us']
+    }";
   }
 }
---
+++ OSHA/CrucibleTests/TranslatorTests/JObjectTranslatorTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using SchemaForge.Crucible;
+using SchemaForge.Crucible.Extensions;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static SchemaForge.Crucible.Constraints;
+using OSHA.TestUtilities;
+
+namespace TranslatorTests
+{
+  [Trait("Crucible", "")]
+  public class JObjectTranslatorTests
+  {
+    private readonly ITestOutputHelper output;
+
+    public JObjectTranslatorTests(ITestOutputHelper output)
+    {
+      this.output = output;
+    }
+
+    /// <summary>
+    /// Tests the capabilites of TryCastToken using reflection.
+    /// </summary>
+    /// <param name="expectedResult">Expected result of the conversion; true if conversion should succeed, false if it should not.</param>
+    /// <param name="type">Full name of the type to attempt to which <paramref name="valueToConvert"/> will be cast.</param>
+    /// <param name="valueToConvert">Value to cast to <paramref name="type"/></param>
+    [Theory]
+    [InlineData(true,"System.String","I'm a string!")]
+    [InlineData(false,"System.Int32","I'm still a string!")]
+    [InlineData(true,"System.Int32","57")]
+    [InlineData(true,"System.DateTime","2021-10-09")]
+    [InlineData(false,"System.DateTime","20211009")] //This is not a format that DateTime.Parse can recognize.
+    [InlineData(true,"System.DateTime","10202109")] //This format is normally not recognized by DateTime.Parse, but is added in the test by RegisterDateTimeFormat.
+    public void ConversionTests(bool expectedResult, string type, string valueToConvert)
+    {
+      JObjectTranslator testTranslator = new();
+      SchemaForge.Crucible.Utilities.Conversions.RegisterDateTimeFormat("MMyyyydd"); // Add a new recognized DateTime format.
+      MethodInfo methodInfo = typeof(JObjectTranslator).GetMethod("TryCastToken");
+      MethodInfo genericMethodInfo = methodInfo.MakeGenericMethod(Type.GetType(type));
+      Assert.Equal(expectedResult, genericMethodInfo.Invoke(testTranslator, new object[] { new JObject() { { "TestToken", valueToConvert } }, "TestToken", null }));
+    }
+
+    /// <summary>
+    /// Tests the <see cref="JObjectTranslator.TokenIsNullOrEmpty(JObject, string)"/>
+    /// method.
+    /// </summary>
+    /// <param name="expectedResult">Expected result of TokenIsNullOrEmpty.</param>
+    /// <param name="testValue">Value to evaluate.</param>
+    [Theory]
+    [InlineData(true,"   ")]
+    [InlineData(false,"I'm here, I'm here!")]
+    public void TokenIsNullOrEmptyTest(bool expectedResult, string testValue)
+    {
+      JObjectTranslator testTranslator = new();
+      Assert.Equal(expectedResult,testTranslator.TokenIsNullOrEmpty(new JObject() { { "TestToken", testValue } }, "TestToken"));
+    }
+
+    /// <summary>
+    /// Tests the <see cref="JObjectTranslator.InsertToken{TDefaultValueType}(JObject, string, TDefaultValueType)"/>
+    /// method by inserting a token.
+    /// </summary>
+    [Fact]
+    public void InsertTokenTest()
+    {
+      JObjectTranslator testTranslator = new();
+      JObject testObject = new() { { "TestToken", "I'm a token!" } };
+      testTranslator.InsertToken(testObject, "Another token", "Me too!");
+      Assert.True(testObject.ContainsKey("Another token"));
+      Assert.True(testObject["Another token"].Value<string>() == "Me too!");
+    }
+
+    /// <summary>
+    /// Tests the <see cref="JObjectTranslator.CollectionContains(JObject, string)"/>
+    /// method.
+    /// </summary>
+    [Fact]
+    public void CollectionContainsTest()
+    {
+      JObjectTranslator testTranslator = new();
+      JObject testObject = new() { { "TestToken", "I'm a token!" } };
+      Assert.True(testTranslator.CollectionContains(testObject, "TestToken"));
+      Assert.False(testTranslator.CollectionContains(testObject, "You thought it was a real token but it was me, Dio!"));
+    }
+
+    /// <summary>
+    /// Tests the <see cref="JObjectTranslator.GetCollectionKeys(JObject)"/>
+    /// method.
+    /// </summary>
+    [Fact]
+    public void GetCollectionKeysTest()
+    {
+      JObjectTranslator testTranslator = new();
+      JObject testObject = new() { { "TestToken", "I'm a token!" }, { "SecondTestToken", "Me too!" },{ "ThirdTestToken", "Me... three?" } };
+      List<string> expectedList = new() { "TestToken", "SecondTestToken", "ThirdTestToken" };
+      List<string> resultList = testTranslator.GetCollectionKeys(testObject);
+      output.WriteLine($"Expected list: {expectedList.Join(", ")}");
+      output.WriteLine($"Actual list: {resultList.Join(", ")}");
+
+      Assert.Equal(expectedList, resultList);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="JObjectTranslator.CollectionValueToString(JObject, string)"/>
+    /// method.
+    /// </summary>
+    [Fact]
+    public void CollectionValueToStringTest()
+    {
+      JObjectTranslator testTranslator = new();
+      JObject testObject = new() { { "TestToken", 38 } };
+      Assert.Equal("38",testTranslator.CollectionValueToString(testObject,"TestToken"));
+    }
+
+    [Fact]
+    public void GetEquivalentTypeTest()
+    {
+      JObjectTranslator testTranslator = new();
+      Assert.Equal("Json Number", testTranslator.GetEquivalentType("Int32"));
+    }
+  }
+}
